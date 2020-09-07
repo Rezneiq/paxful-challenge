2c1a766240d26e25394b2cd7a58d94447983d56e
Federico Ribero <fede.ribero.94@gmail.com>
2020-09-07 05:22:50 -0300

    removed auth statistics

--- challenge/statistic/views.py
+++ challenge/statistic/views.py
@@ -1,16 +1,15 @@
 from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated #, IsAdminUser
 from rest_framework.response import Response
 from transactions.models import Transaction
-# from statistic.serializers import StatisticsSerializer
 from django.db.models import Sum
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.authentication import TokenAuthentication
 from utils.conversor import RatesConversor
 
 
 class StatisticApiView(APIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]          # ,IsAdminUser] I removed this permission in order to avoid the "hardcoded token"
 
     def get(self, request):
         queryset = Transaction.objects.all()
